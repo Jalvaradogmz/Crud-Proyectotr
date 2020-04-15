fdcf496e9048787b5d6d635306013028fbbc19ef
Jalvaradogmz <junioralvaradogmz@gmail.com>
2020-04-14 22:37:44 -0600

    a

--- crud-proyectotr/estudiantes.sql
+++ crud-proyectotr/estudiantes.sql
@@ -2,12 +2,12 @@ create database crudproyectotr;
 use crudproyectotr;
 
 CREATE TABLE `estudiantes` (
-  `Nombre` varchar(30) NOT NULL,
-  `Apellidos` varchar(30) NOT NULL,
-  `Edad` int(30) NOT NULL,
-  `Cedula` int(30) NOT NULL,
-  `Correo` varchar(30) NOT NULL,
-  `Telefono` int(30) NOT NULL,
-  `Fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp()
+  `nombre` varchar(30) NOT NULL,
+  `apellidos` varchar(30) NOT NULL,
+  `edad` int(30) NOT NULL,
+  `cedula` int(30) NOT NULL,
+  `correo` varchar(30) NOT NULL,
+  `telefono` int(30) NOT NULL,
+  `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp()
 );
 
